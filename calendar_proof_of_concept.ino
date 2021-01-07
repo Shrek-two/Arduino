e3076b46308ea91f5a9591b670f3e29000077616
Calvin Hoffmann <hoff1352@umn.edu>
2021-01-07 00:18:26 -0600

    initial commit

---
+++ calendar_proof_of_concept.ino
@@ -0,0 +1,18 @@
+
+// proof of concept for the one year calendar project.
+// let0 january 6 2021
+
+int day1Pin = 6;
+int day2Pin = 5;
+int dailyPinRed = 11;
+int dailyPinGreen = 10;
+int dailyPinBlue = 9;
+int button = 2;
+
+void setup(){
+    
+}
+
+void loop(){
+
+}
