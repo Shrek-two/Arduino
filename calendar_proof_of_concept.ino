f687311df68c8d11e53c55fb379845fff32f4ed6
Calvin Hoffmann <hoff1352@umn.edu>
2021-01-07 13:41:40 -0600

    finished rev1

--- calendar_proof_of_concept.ino
+++ calendar_proof_of_concept.ino
@@ -9,9 +9,11 @@ int dailyPinGreen = 10;
 int dailyPinBlue = 9;
 int button = 2;
 
-void setup()
+int dayCount = 0;
+
+void setup()                // initialize button and lights
 {
-    pinMode(button, INPUT_PULLUP);
+    pinMode(button, INPUT_PULLUP);          
     pinMode(day2Pin, OUTPUT);
     pinMode(day1Pin, OUTPUT);
     pinMode(dailyPinBlue, OUTPUT);
@@ -26,5 +28,18 @@ void setup()
 
 void loop()
 {
-    
+    #define daily_delay 864000  // delay time for one day
+
+    if(digitalRead(button) == LOW){             // watch for button press
+        digitalWrite(dailyPinGreen, HIGH);      // if button is pressed, set light to green
+        digitalWrite(dailyPinRed, LOW);         // unset red
+
+        if(dayCount == 0){
+            digitalWrite(day1Pin, HIGH);
+        }
+        if(dayCount == 1){
+            digitalWrite(day2Pin, HIGH);            
+        }
+
+    }
 }
