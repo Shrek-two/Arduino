2e842c44418b432eeec0b0abb0913ea6858efbb8
Calvin Hoffmann <hoff1352@umn.edu>
2021-01-08 12:19:58 -0600

    proof of concept complete

--- calendar_proof_of_concept/calendar_proof_of_concept.ino
+++ calendar_proof_of_concept/calendar_proof_of_concept.ino
@@ -6,22 +6,23 @@ int day1Pin = 6;
 int day2Pin = 5;
 int dailyPinRed = 11;
 int dailyPinGreen = 10;
-int dailyPinBlue = 9;
 int button = 2;
+int resetButton = 4;
 
 int dayCount = 0;
 
+int lights [2] = {day1Pin, day2Pin};
+
 void setup()                // initialize button and lights
 {
-    pinMode(button, INPUT_PULLUP);          
+    pinMode(button, INPUT_PULLUP);    
+    pinMode(resetButton, INPUT_PULLUP);        
     pinMode(day2Pin, OUTPUT);
     pinMode(day1Pin, OUTPUT);
-    pinMode(dailyPinBlue, OUTPUT);
     pinMode(dailyPinGreen, OUTPUT);
     pinMode(dailyPinRed, OUTPUT);
 
     digitalWrite(dailyPinRed, HIGH);
-    digitalWrite(dailyPinBlue, LOW);
     digitalWrite(dailyPinGreen, LOW);
 
 }
@@ -34,14 +35,15 @@ void loop()
         digitalWrite(dailyPinGreen, HIGH);      // if button is pressed, set light to green
         digitalWrite(dailyPinRed, LOW);         // unset red
 
-        if(dayCount == 0){
-            digitalWrite(day1Pin, HIGH);
-        }
-        if(dayCount == 1){
-            digitalWrite(day2Pin, HIGH);            
-        }
+        digitalWrite(lights[dayCount], HIGH);
 
         dayCount++;
     }
     delay(250);
+
+    if(digitalRead(resetButton) == LOW){
+        digitalWrite(dailyPinGreen, LOW);      // if button is pressed, unset light to green
+        digitalWrite(dailyPinRed, HIGH);         // set red
+    }
+
 }
